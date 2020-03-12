d59029d12db76bf3ed268c23f7a6fde3fc1a9256
Jake Clark <jclark208@yahoo.com>
2020-03-12 12:16:02 -0400

    MVP done

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+  test('Does contact form render without crashing',() => {
+    render(<ContactForm />)
+  });
+
+  test('Does the button have the type of submit', () => {
+    const {getByText} = render(<App />);
+    const email = getByText(/email*/i);
+    expect(email).toBeInTheDocument();
+  });
